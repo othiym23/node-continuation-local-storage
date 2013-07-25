4043e42628fdb43aab1771e3241472e576c1f2a7
Forrest L Norvell <forrest@newrelic.com>
2013-07-25 15:01:18 -0700

    Nesting is much simpler with the new API.

--- test/nesting.tap.js
+++ test/nesting.tap.js
@@ -3,69 +3,61 @@
 var tap = require('tap');
 var test = tap.test;
 
-var context = require('../context.js');
+var cls = require('../context.js');
 
 test("nested contexts on a single namespace", function (t) {
   t.plan(7);
 
-  var namespace = context.createNamespace("namespace");
-  var toplevel = namespace.createContext();
-  namespace.set("value", 1);
+  var namespace = cls.createNamespace("namespace");
+  namespace.run(function () {
+    namespace.set("value", 1);
 
-  t.equal(namespace.get("value"), 1,
-          "namespaces have associated data even without contexts.");
-  toplevel.on('end', function () {
     t.equal(namespace.get("value"), 1,
-            "namespace retains its outermost value.");
-  });
+            "namespaces have associated data even without contexts.");
 
-  toplevel.run(function () {
-    t.equal(namespace.get("value"), 1, "lookup will check enclosing context");
-    namespace.set("value", 2);
-    t.equal(namespace.get("value"), 2, "setting works on top-level context");
-    var nested = namespace.createContext();
+    namespace.run(function () {
+      t.equal(namespace.get("value"), 1, "lookup will check enclosing context");
+      namespace.set("value", 2);
+      t.equal(namespace.get("value"), 2, "setting works on top-level context");
 
-    nested.on('end', function () {
+      namespace.run(function () {
+        t.equal(namespace.get("value"), 2, "lookup will check enclosing context");
+        namespace.set("value", 3);
+        t.equal(namespace.get("value"), 3, "setting works on nested context");
+      });
+
       t.equal(namespace.get("value"), 2,
               "should revert to value set in top-level context");
     });
 
-    nested.run(function () {
-    t.equal(namespace.get("value"), 2, "lookup will check enclosing context");
-      namespace.set("value", 3);
-      t.equal(namespace.get("value"), 3,
-              "setting works on nested context");
-    });
+    t.equal(namespace.get("value"), 1, "namespace retains its outermost value.");
   });
 });
 
 test("the example from the docs", function (t) {
-  var writer = context.createNamespace('writer');
+  var writer = cls.createNamespace('writer');
   writer.set('value', 0);
 
+  t.equal(writer.get('value'), 0, "outer hasn't been entered yet");
   function requestHandler() {
-    var outer = writer.createContext();
-    t.equal(writer.get('value'), 0, "outer hasn't been entered yet");
-    outer.run(function () {
+    writer.run(function (outer) {
       t.equal(writer.active, outer, "writer.active == outer");
 
       writer.set('value', 1);
-      t.equal(outer.get('value'), 1, "outer is active");
       t.equal(writer.get('value'), 1, "writer.active == outer");
-
-      var inner = writer.createContext();
+      t.equal(outer.value, 1, "outer is active");
 
       process.nextTick(function () {
         // FIXME: once this is in core, 'value' should be 1 because
         // outer is still active
         // FIXME: t.equal(writer.active, outer, "writer.active == outer");
         t.equal(writer.get('value'), 0, "inner hasn't been entered yet");
-        inner.run(function () {
+        writer.run(function (inner) {
           t.equal(writer.active, inner, "writer.active == inner");
 
           writer.set('value', 2);
-          t.equal(outer.get('value'), 1, "outer is unchanged");
-          t.equal(inner.get('value'), 2, "inner is active");
+          t.equal(outer.value, 1, "outer is unchanged");
+          t.equal(inner.value, 2, "inner is active");
           t.equal(writer.get('value'), 2, "writer.active == inner");
         });
       });
