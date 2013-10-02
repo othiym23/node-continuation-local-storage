fed658303d246f2a22fbb21a24344d0e4df003f2
Forrest L Norvell <forrest@newrelic.com>
2013-10-02 13:22:06 -0700

    test: don't use setImmediate in tests
    Unless it's necessary, don't use 0.8+ idioms so the test suite can be
    run under 0.6.x.

--- test/async-no-run-queue-multiple.tap.js
+++ test/async-no-run-queue-multiple.tap.js
@@ -7,7 +7,7 @@ test("minimized test case that caused #6011 patch to fail", function (t) {
 
   console.log('+');
   // when the flaw was in the patch, commenting out this line would fix things:
-  setImmediate(function () { console.log('!'); });
+  process.nextTick(function () { console.log('!'); });
 
   var n = cls.createNamespace("test");
   t.ok(!n.get('state'), "state should not yet be visible");
@@ -16,7 +16,7 @@ test("minimized test case that caused #6011 patch to fail", function (t) {
     n.set('state', true);
     t.ok(n.get('state'), "state should be visible");
 
-    setImmediate(function () {
+    process.nextTick(function () {
       t.ok(n.get('state'), "state should be visible");
     });
   });
