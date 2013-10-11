4a98b29ed988ad50afa4ed774b433564cfbfaea4
Forrest L Norvell <forrest@newrelic.com>
2013-10-10 17:28:11 -0700

    test: fix monkeypatching checks
    As long as the core timer methods are wrapped, we don't need to worry
    about wrapping the globally exposed shims. Actually a change in the
    async-listener shim, but until these tests are moved over there,
    maintaining here.

--- test/monkeypatching.tap.js
+++ test/monkeypatching.tap.js
@@ -2,68 +2,55 @@
 var test = require('tap').test;
 
 if (!process.addAsyncListener) {
+  test("overwriting startup.processNextTick", function (t) {
+    t.plan(2);
 
+    t.doesNotThrow(function () { require('../context.js'); });
 
-test("overwriting startup.processNextTick", function (t) {
-  t.plan(2);
+    t.ok(process.nextTick.__wrapped, "should wrap process.nextTick()");
+  });
 
-  t.doesNotThrow(function () { require('../context.js'); });
+  test("overwriting domain helpers", function (t) {
+    // domain helpers were only in 0.10.x
+    if (!(process._nextDomainTick ||
+          process._tickDomainCallback)) {
+      return t.end();
+    }
 
-  t.ok(process.nextTick.__wrapped, "should wrap process.nextTick()");
-});
+    t.plan(2);
 
-test("overwriting domain helpers", function (t) {
-  // domain helpers were only in 0.10.x
-  if (!(process._nextDomainTick ||
-        process._tickDomainCallback)) {
-    return t.end();
-  }
+    t.ok(process._nextDomainTick.__wrapped,
+         "should wrap process._nextDomainTick()");
+    t.ok(process._tickDomainCallback.__wrapped,
+         "should wrap process._tickDomainCallback()");
+  });
 
-  t.plan(2);
+  test("overwriting timers", function (t) {
+    t.plan(2);
 
-  t.ok(process._nextDomainTick.__wrapped,
-       "should wrap process._nextDomainTick()");
-  t.ok(process._tickDomainCallback.__wrapped,
-       "should wrap process._tickDomainCallback()");
-});
+    var timers = require('timers');
+    t.ok(timers.setTimeout.__wrapped, "should wrap setTimeout()");
+    t.ok(timers.setInterval.__wrapped, "should wrap setInterval()");
 
-test("overwriting timers", function (t) {
-  t.plan(6);
+    /* It would be nice to test that monkeypatching preserves the status quo
+     * ante, but assert thinks setTimeout !== global.setTimeout (why?) and both of
+     * those are a wrapper around NativeModule.require("timers").setTimeout,
+     * presumably to try to prevent the kind of "fun" I'm having here.
+     */
+  });
 
-  t.ok(setTimeout.__wrapped, "should wrap setTimeout()");
-  t.ok(setInterval.__wrapped, "should wrap setInterval()");
+  test("overwriting setImmediate", function (t) {
+    // setTimeout's a johnny-come-lately
+    if (!global.setImmediate) return t.end();
 
-  t.ok(global.setTimeout.__wrapped, "should also wrap global setTimeout()");
-  t.ok(global.setInterval.__wrapped, "should also wrap global setInterval()");
+    t.plan(1);
 
-  var timers = require('timers');
-  t.ok(timers.setTimeout.__wrapped, "should wrap setTimeout()");
-  t.ok(timers.setInterval.__wrapped, "should wrap setInterval()");
+    t.ok(require('timers').setImmediate.__wrapped, "should wrap setImmediate()");
 
-  /* It would be nice to test that monkeypatching preserves the status quo
-   * ante, but assert thinks setTimeout !== global.setTimeout (why?) and both of
-   * those are a wrapper around NativeModule.require("timers").setTimeout,
-   * presumably to try to prevent the kind of "fun" I'm having here.
-   */
-});
-
-test("overwriting setImmediate", function (t) {
-  // setTimeout's a johnny-come-lately
-  if (!global.setImmediate) return t.end();
-
-  t.plan(3);
-
-  t.ok(setImmediate.__wrapped, "should wrap setImmediate()");
-  t.ok(global.setImmediate.__wrapped, "should also wrap global setImmediate()");
-  t.ok(require('timers').setImmediate.__wrapped, "should wrap setImmediate()");
-
-  /* It would be nice to test that monkeypatching preserves the status quo
-   * ante, but assert thinks setTimeout !== global.setTimeout (why?) and both of
-   * those are a wrapper around NativeModule.require("timers").setTimeout,
-   * presumably to try to prevent the kind of "fun" I'm having here.
-   */
-
-});
-
-
+    /* It would be nice to test that monkeypatching preserves the status quo
+     * ante, but assert thinks setTimeout !== global.setTimeout (why?) and both of
+     * those are a wrapper around NativeModule.require("timers").setTimeout,
+     * presumably to try to prevent the kind of "fun" I'm having here.
+     */
+  });
 }
